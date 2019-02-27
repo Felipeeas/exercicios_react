41d2afe9d19abe473b3e6f9a2465911f02231406
Felipe Eduardo <felipeeduardoavelino2@gmail.com>
2019-02-27 19:04:37 -0300

    up exercicios finais

--- ex.8/index.jsx
+++ ex.8/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Field from './field'
+import ClassComponent from './classComponent'
 
 ReactDOM.render(
     
-    <Field initialValue='Teste'/>
+    <ClassComponent initialValue={10} label= 'Contador'/>
 
 
 , document.getElementById('app'))
